71f65b6020947d573d2d3566ae32e7f75d1aa872
Amin Rais <muhammadamin.rais1@gmail.com>
2022-09-20 19:47:00 +0700

    add insertUser

--- features/user/delivery/requset.go
+++ features/user/delivery/requset.go
@@ -1,5 +1,7 @@
 package delivery
 
+import "project/dashboard/features/user"
+
 type Request struct {
 	ID       uint   `json:"id" form:"id"`
 	Fullname string `json:"fullname" form:"fullname"`
@@ -8,4 +10,15 @@ type Request struct {
 	Team     string `json:"team" form:"team"`
 	Role     string `json:"role" form:"role"`
 	Status   string `json:"status" form:"status"`
+}
+
+func (req *Request) toCoreReq() user.Core {
+	return user.Core{
+		Fullname: req.Fullname,
+		Email:    req.Email,
+		Password: req.Password,
+		Team:     req.Team,
+		Role:     req.Role,
+		Status:   req.Status,
+	}
 }
